416977eb468f017c3b6c1a9f4ccc1fa7cdab2844
fr0st <fr0st1s747@gmail.com>
2024-10-31 19:52:21 +0300

    homework_14_1

--- oop/17.1_main.py
+++ oop/17.1_main.py
@@ -28,17 +28,13 @@ if __name__ == '__main__':
     else:
         print("Не возникла ошибка ValueError при попытке добавить продукт с нулевым количеством")
 
-
     product1 = Product("Samsung Galaxy S23 Ultra", 5, 180000.0)
     product2 = Product("Iphone 15", 8, 210000.0)
     product3 = Product("Xiaomi Redmi Note 11", 14, 31000.0)
 
-
     category1 = Category("Смартфоны", "Категория смартфонов", [product1, product2, product3])
 
-
     print("Средняя цена в категории 'Смартфоны':", category1.average_price())
-
 
     category_empty = Category("Пустая категория", "Категория без продуктов", [])
     print("Средняя цена в пустой категории:", category_empty.average_price())
